8c93d8d8c8ea795a6833a196ed80e5b5122ff136
gummy52 <stevenrogers52software@gmail.com>
2015-04-15 00:19:03 -0400

    Read from a file for credentials

--- src/TwitchTriviaBot.cpp
+++ src/TwitchTriviaBot.cpp
@@ -4,12 +4,19 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	TwitchIRC irc("gummy52", "gummy52", "irc.twitch.tv", 6667, "#gummy52", "oauth:k5lda5anbr86bpfxhzofw3sjxlomxy");
+	std::vector<std::string> config = Util::readLinesFromFile("login.txt");
 
-	while (irc.activeSocket())
+	if (config.size() >= 5)
 	{
-		irc.Update();
+		TwitchIRC irc(config[0], config[0], config[1], (uint32)atoi(config[2].c_str()), config[3], config[4]);
+
+		while (irc.activeSocket())
+		{
+			irc.Update();
+		}
 	}
+	else
+		printf("There was an issue reading login.txt.\n");
 
 	printf("The program has ended.\n");
 	system("pause");
