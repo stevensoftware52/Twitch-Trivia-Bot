4042469f4fe511ceda07af3cfa87fb1a3b392967
Steven Rogers <stevenrogers52software@gmail.com>
2015-04-15 18:05:02 -0400

    Saving/Loading of a scoreboard as well loading of formatted trivia questions, answers and the points awarded.

--- src/Trivia.cpp
+++ src/Trivia.cpp
@@ -7,4 +7,76 @@ Trivia::Trivia(TwitchIRC* pOwner) :
 {
 	// Need a TwitchIRC for this class to work
 	ASSERT(m_pOwner);
+
+	LoadScore();
+	LoadTrivia();
+}
+
+void Trivia::LoadTrivia()
+{
+	m_vQuestions.clear();
+
+	// Question
+	// Answer1\tAnswer2\tAnswer3, etc
+	// Points awarded
+
+	std::vector<std::string> lines = Util::readLinesFromFile("trivia.txt");
+
+	for (size_t i = 0; i < lines.size(); i += 4)
+	{
+		if (i + 2 >= lines.size())
+		{
+			printf("Trivia::LoadTrivia - There was an issue at line %d.\n", i);
+			break;
+		}
+
+		TriviaQA trivia;
+		trivia.question = lines[i];
+		getWordsSeperatedBy(lines[i + 1], TRIVIA_WORDS_SEPERATOR, trivia.answers);
+		trivia.points = (uint8)atoi(lines[i + 2].c_str());
+
+		if (!trivia.question.size())
+			printf("Trivia::LoadTrivia - Invalid question at line %d.\n", i);
+
+		if (!trivia.answers.size())
+			printf("Trivia::LoadTrivia - Invalid answers at line %d.\n", i);
+
+		if (!trivia.points)
+			printf("Trivia::LoadTrivia - Invalid points at line %d.\n", i);
+
+		m_vQuestions.push_back(trivia);
+	}
+
+	printf("Loaded %d trivia questions.\n", m_vQuestions.size());
+}
+
+void Trivia::SaveScore()
+{
+    std::ofstream file("scoreBoard.txt");
+
+	if (file.is_open())
+	{
+		for (auto itr = m_uoScore.begin(); itr != m_uoScore.end(); ++itr)
+			file << itr->first << TRIVIA_WORDS_SEPERATOR << itr->second << "\n";
+
+		file.close();
+	}
+}
+
+void Trivia::LoadScore()
+{
+	std::vector<std::string> scores = Util::readLinesFromFile("scoreboard.txt");
+
+	for (size_t i = 0; i < scores.size(); ++i)
+	{
+		std::vector<std::string> words;
+		getWordsSeperatedBy(scores[i], TRIVIA_WORDS_SEPERATOR, words);
+
+		if (words.size() == 2)
+			m_uoScore[words[0]] = (uint32)atoi(words[1].c_str());
+		else
+			printf("Trivia::LoadScore - Issue reading score at line %d, found %d words\n", i, words.size());
+	}
+
+	SaveScore();
 }
--- src/Trivia.h
+++ src/Trivia.h
@@ -7,16 +7,59 @@
 
 class TwitchIRC;
 
+// The letter that separates one phrase from another
+//		Example,	"One	Two	Three	Four	Five	etc..."	
+#define TRIVIA_WORDS_SEPERATOR '\t'
+
+struct TriviaQA
+{
+	TriviaQA() :
+		points(1)
+	{}
+
+	std::string question;
+	std::vector<std::string> answers;
+	uint8 points;
+};
+
 class Trivia
 {
 	public:
 		Trivia(TwitchIRC* pOwner);
 		virtual ~Trivia() {}
 
+		static void getWordsSeperatedBy(const std::string line, const char seperator, std::vector<std::string>& result)
+		{
+			std::string content;
+
+			for (size_t i = 0; i < line.size(); ++i)
+			{
+				if (line[i] == seperator)
+				{
+					result.push_back(content);
+					content.clear();
+				}
+				else
+				{
+					content.push_back(line[i]);
+				}
+			}
+
+			if (content.size())
+				result.push_back(content);
+		}
+
 	private:
 		void LoadScore();
+		void SaveScore();
+		void LoadTrivia();
 
 		TwitchIRC* m_pOwner;
+
+		std::vector<TriviaQA> m_vQuestions;
+
+		// name][score
+		std::unordered_map<std::string, uint32> m_uoScore;
 };
 
 #endif
